e99670d3c9b2997617ac2bf5365559fc7b240c7c
Adetunji <idowuolatunde32@gmail.com>
2023-02-21 15:42:51 +0100

    modal added to the card page

--- src/components/CardPage.jsx
+++ src/components/CardPage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-import { Box,Container,CardHeader,Card, CardBody, CardFooter,SimpleGrid,Text,Image, Center,Divider } from '@chakra-ui/react'
+import { Box,Container,CardHeader,Card, CardBody, CardFooter,SimpleGrid,Text,Image, Center,Divider,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button
+
+} from '@chakra-ui/react'
 import one from './Images/pic.png'
 
-const CardPage = () => {
+export default function CardPage () {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  
   return (
     <Container>
       <Center>
@@ -32,9 +46,49 @@ const CardPage = () => {
           </Box>
         </CardBody>
         <Divider color={' #97d1f3'} />
-        <CardFooter className='CardFooter' justify={'center'} >
-          <Text  fontSize={'20px'} textDecorationLine='underline' color={'#0555A8'} fontFamily='HafferXH,sans-serif'>More details</Text>
+        <CardFooter className='CardFooter' justify={'center'} onClick={onOpen} >
+          <Button  fontSize={'20px'}
+          textDecorationLine='underline'
+          color={'#0555A8'}
+          fontFamily='HafferXH,sans-serif'
+          backgroundColor={'transparent'}
+          _hover={{
+            bg: 'transparent',
+          }}>More details</Button>
         </CardFooter>
+        <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent borderRadius={'16px'}>
+          <ModalHeader display={'flex'} textAlign='center' justifyItems={'center'}>
+            <Image padding={'0'} width={'54px'} height={'44px'} src={one}/>
+            <Text>SMS</Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <Divider color={' #05334f'} />
+          <ModalBody display={'flex'} justifyContent='space-between' paddingTop={'34px'}>
+            <Text>To Send SMS</Text>
+            <Text>To Receive SMS</Text>
+
+          </ModalBody>
+          <ModalFooter display={'flex'} justifyContent='space-between' backgroundColor={'#EFF4FE'} margin='20px'>
+            <Box>
+              <Text fontSize={'16px'} paddingTop='8px'>To Send SMS</Text>
+              <Text  fontSize={'16px'} marginTop='11px'><span>NGN 2.4255</span>/sms</Text>
+            </Box>
+            <Text bgColor={'#EDF5FE'} color='#EFF4FE' textAlign={'center'}
+              marginTop='8px'
+              paddingBottom={'4px'}
+              paddingTop={'4px'}
+              paddingLeft={'12px'}
+              paddingRight={'12px'}
+              borderRadius={'12px'}
+              fontSize='12px'
+              fontFamily={'"DM Sans"'}
+              backgroundColor='#97d1f3'
+              >Coming Soon</Text>
+          </ModalFooter>
+        </ModalContent>
+        </Modal>
       </Card>
       <Card  className='Card' width={{md:'398px', base:'380px'}}margin={{md:'full', base:'0 auto'}} marginBottom={{base:'2rem'}}>
         <CardHeader>
@@ -98,9 +152,49 @@ const CardPage = () => {
           </Box>
         </CardBody>
         <Divider color={' #97d1f3'} />
-        <CardFooter className='CardFooter' justify={'center'} >
-          <Text  fontSize={'20px'} textDecorationLine='underline' color={'#0555A8'} fontFamily='HafferXH,sans-serif'>More details</Text>
+        <CardFooter className='CardFooter' justify={'center'} onClick={onOpen} >
+          <Button  fontSize={'20px'}
+          textDecorationLine='underline'
+          color={'#0555A8'}
+          fontFamily='HafferXH,sans-serif'
+          backgroundColor={'transparent'}
+          _hover={{
+            bg: 'transparent',
+          }}>More details</Button>
         </CardFooter>
+        <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent borderRadius={'16px'}>
+          <ModalHeader display={'flex'} textAlign='center' justifyItems={'center'}>
+            <Image padding={'0'} width={'54px'} height={'44px'} src={one}/>
+            <Text>SMS</Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <Divider color={' #05334f'} />
+          <ModalBody display={'flex'} justifyContent='space-between' paddingTop={'34px'}>
+            <Text>To Send Message</Text>
+            <Text>To Receive Message</Text>
+
+          </ModalBody>
+          <ModalFooter display={'flex'} justifyContent='space-between' backgroundColor={'#EFF4FE'} margin='20px'>
+            <Box>
+              <Text fontSize={'16px'} paddingTop='8px'>Start at</Text>
+              <Text  fontSize={'16px'} marginTop='11px'><span>NGN 3.5000/msg</span>/sms</Text>
+            </Box>
+            <Text bgColor={'#EDF5FE'}  textAlign={'center'}
+              marginTop='8px'
+              paddingBottom={'4px'}
+              paddingTop={'4px'}
+              paddingLeft={'12px'}
+              paddingRight={'0px'}
+              borderRadius={'12px'}
+              fontSize='16px'
+              fontFamily={'"DM Sans"'}
+              fontWeight='bold'
+              >NGN 0.0000/msg</Text>
+          </ModalFooter>
+        </ModalContent>
+        </Modal>
       </Card>
       <Card  className='Card' width={{md:'398px', base:'380px'}}margin={{md:'full', base:'0 auto'}} >
         <CardHeader>
@@ -134,4 +228,4 @@ const CardPage = () => {
   )
 }
 
-export default CardPage
+
